bdd37f46e41437c01ca2b9a030aaa76db95884dc
Poplavskiy A. A <rainfire491@gmail.com>
2022-05-17 18:59:44 +1100

    add text PROD

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -25,7 +25,8 @@
         <p>Ваш ip: {{ $ip }}</p>
         <p>User agent: {{ $data2 }}</p>
         <?= var_dump($data1);?>
-<!--        --><?//= //phpinfo(); ?>
+
+        <h1>PROD</h1>
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
