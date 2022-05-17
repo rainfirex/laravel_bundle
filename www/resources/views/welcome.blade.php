5c8f7855858dc469d6867b0e6e7851e24d2a739f
Poplavskiy A. A <rainfire491@gmail.com>
2022-05-17 19:08:27 +1100

    add text DEV

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <p>Ваш ip: {{ $ip }}</p>
         <p>User agent: {{ $data2 }}</p>
         <?= var_dump($data1);?>
-<!--        --><?//= //phpinfo(); ?>
+        <h1>DEV</h1>
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
